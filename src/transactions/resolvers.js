fe16aabcae0960e142348737b868998b77f942ca
FelipeH22 <juan22herrera01@gmail.com>
2023-10-02 20:23:18 -0500

    Finished

--- src/transactions/resolvers.js
+++ src/transactions/resolvers.js
@@ -1,20 +1,61 @@
 import { generalRequest, getRequest, getCleanRequest } from '../utilities';
-import { url, port, entryPointUsers } from './server';
+import { url, port, entryPointUsers, entryPointRecharges, entryPointPayments, entryPointInternalTransactions, entryPointBalances, entryPointMovements } from './server';
 
-const URLtransactions = `https://${url}:${port}/${entryPointUsers}`;
+const URLusers = `https://${url}:${port}/${entryPointUsers}`;
+const URLrecharge = `https://${url}:${port}/${entryPointRecharges}`;
+const URLpayment = `https://${url}:${port}/${entryPointPayments}`;
+const URLinternalTransactions = `https://${url}:${port}/${entryPointInternalTransactions}`;
+const URLbalance = `https://${url}:${port}/${entryPointBalances}`;
+const URLmovement = `https://${url}:${port}/${entryPointMovements}`;
 
 const resolvers = {
 	Query: {
 		allUsers: (_) =>
-			getCleanRequest(URLtransactions, ''),
+			getCleanRequest(URLusers, ''),
 		userById: (_, { id_user }) =>
-			generalRequest(`${URLtransactions}/${id_user}`, 'GET'),
+			generalRequest(`${URLusers}/${id_user}`, 'GET'),
+
+		
+		allRecharges: (_) =>
+			getCleanRequest(URLrecharge, ''),
+		rechargeById: (_, { id_recharge }) =>
+			generalRequest(`${URLrecharge}/${id_recharge}`, 'GET'),
+
+			
+		allPayments: (_) =>
+			getCleanRequest(URLpayment, ''),
+		paymentById: (_, { id_payment }) =>
+		generalRequest(`${URLpayment}/${id_payment}`, 'GET'),
+
+		allInternalTransactions: (_) =>
+			getCleanRequest(URLinternalTransactions, ''),
+		internalTransactionById: (_, { id_internal_transactions }) =>
+		generalRequest(`${URLinternalTransactions}/${id_internal_transactions}`, 'GET'),
+
+		allBalances: (_) =>
+			getCleanRequest(URLbalance, ''),
+		balanceByUserId: (_, { id_user }) =>
+			generalRequest(`${URLbalance}/${id_user}`, 'GET'),
+
+		allMovements: (_) =>
+			getCleanRequest(URLmovement, '')
+		
+
 
 
 	},
 	Mutation: {
 		createUser: (_, { user }) =>
-			generalRequest(`${URLtransactions}`, 'POST', user)
+			generalRequest(`${URLusers}`, 'POST', user),
+		
+		createRecharge: (_, { recharge }) =>
+			generalRequest(`${URLrecharge}`, 'POST', recharge),
+
+		createPayment: (_, { payment }) =>
+			generalRequest(`${URLpayment}`, 'POST', payment),
+
+		createInternalTransaction: (_, { internal_transaction }) =>
+			generalRequest(`${URLinternalTransactions}`, 'POST', internal_transaction),
 	}
 };
 
