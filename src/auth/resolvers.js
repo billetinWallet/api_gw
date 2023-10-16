26b9135a84effffd7b608a930bc13855e961043c
FelipeH22 <juan22herrera01@gmail.com>
2023-10-16 13:17:32 -0500

    auth connection

--- src/auth/resolvers.js
+++ src/auth/resolvers.js
@@ -1,12 +1,12 @@
 import { generalRequest, getRequest, getCleanRequest } from '../utilities';
 import { url, port, entryPointAuth} from './server';
 
-const URLauth = `http://${url}:${port}/${entryPointAuth}`;
+const URLauth = `https://${url}:${port}/${entryPointAuth}`;
 
 const resolvers = {
 	Query: {
 		getUser: (_) =>
-			getRequest(`http://${url}:${port}/`, 'GET')
+			getRequest(`https://${url}:${port}/`, 'GET')
 	},
 	Mutation: {
 		newUser: (_, { User }) =>
